02dd3971eeea6a8d54af4d0e4ee33b23cef33fdf
whoisnian <zhuchangbao2017@gmail.com>
2017-05-02 22:03:17 +0800

    upload/index

---
+++ upload/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>
+		WhiteWings</title></head>
+<body oncontextmenu=self.event.returnValue=false style="background-color:#000000;" >
+	<br/>
+	<br/>
+	<h1 style="font-family:verdana;text-align:center;color:#ff0000" >
+		Permission denied
+	</h1>
+	<p style="font-size:25px;text-align:center;color:#ff0000" ><b>
+	<?php
+		echo $_SERVER['REMOTE_ADDR'];
+	?>	
+	</b></p>
+</body>
+</html>
