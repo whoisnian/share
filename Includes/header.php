07776ceb63d7d89d438044017e14eca3d680e860
whoisnian <zhuchangbao2017@gmail.com>
2017-11-18 01:56:19 +0800

    android chrome app mode

--- Includes/header.php
+++ Includes/header.php
@@ -5,7 +5,11 @@ echo '
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+        <meta name="mobile-web-app-capable" content="yes">
         <title>Share</title>
+
+        <link rel="shortcut icon" href="/favicon.png">
+        <link rel="icon" sizes="192x192" href="/icon.png">
         <link rel="stylesheet" type="text/css" href="/Styles/nian.css">
     </head>
 
